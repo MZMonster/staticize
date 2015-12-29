af26bb5ef33eb0bf9e442b94886d5b446ca8d3cc
zhangxun <xtkml.g@gmail.com>
2015-12-29 19:44:15 +0800

    Modify `cacheMiddleware`

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ module.exports = Staticize;
  *
  * @param {Object} options:
  *   1. `options.cache` - such as {adapter: 'redis', config: {host: 'localhost', port: '6379', database: 2}}
- *   2. `options.routes` - such as `{'/index': 60, '/post': 120 }` (cache for seconds, only support `GET` and `HEAD`)
+ *   2. `options.routes` - such as `{'get /index': 60, 'get /post': 120 }` (cache for seconds)
+ *   3. `options.debug` - print debug info
  * @public
  */
 function Staticize(options) {
@@ -41,13 +42,53 @@ function Staticize(options) {
     throw new TypeError('options.routes required');
   }
   this.routes = routes;
+
+  this.debug = options.debug || false;
+
+  // debug info
+  if (this.debug) {
+    console.log('Staticize initial');
+  }
 }
 
+/**
+ * get cache ttl
+ * @param url
+ * @private
+ */
+Staticize.prototype._getCacheTTL = function (url) {
+  if (this.routes[url]) {
+    return this.routes[url];
+  }
+  for (var key in this.routes) {
+    if (url.indexOf(key) > -1) {
+      return this.routes[key];
+    }
+  }
+  return 0;
+};
 /**
  * return cache file or render or api res result if exist
  */
 Staticize.prototype.cacheMiddleware = function (req, res, next) {
-
+  var cacheKey = req.method.toLowerCase() + ' ' + req.originalUrl;
+  // debug
+  if (this.debug) {
+    console.log('get %s from cache', req.originalUrl)
+  }
+  // get from cache
+  this.cache.get(cacheKey)
+    .then(function (data) {
+      if (data) {
+        return res.send(data);
+      } else {
+        return next();
+      }
+    })
+    .catch(function (err) {
+      console.error(err);
+      return next();
+    });
 };
 
 /**
@@ -57,13 +98,6 @@ Staticize.prototype.cacheMiddleware = function (req, res, next) {
  */
 Staticize.prototype.cacheFile = function (root, options) {
 
-  
-
-  return function (req, res, next) {
-    if (req.method !== 'GET' && req.method !== 'HEAD') {
-      return next();
-    }
-  }
 };
 /**
  *
